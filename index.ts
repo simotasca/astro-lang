a798148bd372a0da8fb69a13bd7191a370511251
Simone Tasca <simo.tasca@gmail.com>
2023-09-22 15:42:27 +0200

    lang config export

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { config } from "./src/config";
 export { default as Link } from "./src/components/Link.astro";
 export { default as LangSwitch } from "./src/components/LangSwitch.astro";
-export const lang = { ...config };
+export const langConfig = { ...config };
 export * from "./src/lang";
