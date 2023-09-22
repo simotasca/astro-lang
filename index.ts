3d0df762c8ffcf7d349c197a3775889d894880ee
Simone Tasca <simo.tasca@gmail.com>
2023-09-22 15:35:08 +0200

    export config as lang

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-// Do not write code directly here, instead use the `src` folder!
-// Then, use this file to export everything you want your user to access.
+import { config } from "./src/config";
 
 export { default as Link } from "./src/components/Link.astro";
 export { default as LangSwitch } from "./src/components/LangSwitch.astro";
+export const lang = { ...config };
 export * from "./src/lang";
